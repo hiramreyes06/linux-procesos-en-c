b3e3bf708b59cd8675a49f9f7e34e66476df6268
hiramreyes06 <luishiramreyes@hotmail.com>
2020-09-22 15:50:37 +0000

    primero commit

---
+++ primero.c
@@ -0,0 +1,42 @@
+
+//pid_t = int GNU C es la variable que contiene el process id
+//getpid() = Obtiene el ID del proceso actual
+//getppid() = Obtiene el Id del padre del proceso actual
+/*
+fork() si sale bien
+	El proceso padre nos va a regresar el proceso del hijo
+	Hijo nos regresa un 0
+Si falla
+	El proceso padre me regresa un -1
+	El hijo no se crea
+*/
+
+/*
+
+Para poder compular el codigo ejecutamos el comando
+gcc nombreArchivo.c -o nombreCompilado
+
+Para ejecutar un codigo compilado
+./ynombreDelArchivo
+*/
+
+#include<unistd.h>
+#include<stdio.h>
+
+int main(void){
+
+pid_t procid;
+//Para imprimir un numero en decimal se ocupa %d
+printf("My current process id is %d \n", getpid() );
+
+procid = fork();
+printf("Fork ID %d \n", procid);
+
+
+printf("Calling fork %d \n", getppid());
+
+return 0;
+}
+
+
+
